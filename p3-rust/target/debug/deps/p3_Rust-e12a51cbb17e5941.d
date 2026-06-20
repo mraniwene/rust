e3e6b9954798c662fe0206490c1f846a01127e2a
Aniwene Madolomani <mraniwene@proton.me>
2026-06-21 02:39:19 +0300

    P3 Rust Project

---
+++ p3-rust/target/debug/deps/p3_Rust-e12a51cbb17e5941.d
@@ -0,0 +1,5 @@
+/home/mraniwenecom/Github/Rust/p3-rust/target/debug/deps/p3_Rust-e12a51cbb17e5941.d: src/main.rs
+
+/home/mraniwenecom/Github/Rust/p3-rust/target/debug/deps/p3_Rust-e12a51cbb17e5941: src/main.rs
+
+src/main.rs:
---
+++ p3-rust/target/debug/p3-Rust.d
@@ -0,0 +1,1 @@
+/home/mraniwenecom/Github/Rust/p3-rust/target/debug/p3-Rust: /home/mraniwenecom/Github/Rust/p3-rust/src/main.rs
